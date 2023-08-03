0d5bfcb07845e59988b6ffa29194322ba0e25c7d
poopyjoe234 <139646796+poopyjoe234@users.noreply.github.com>
2023-08-03 13:21:56 -0400

    Started the class coding - maybe learning how to scrape for the item information is quicker? build something to input the information as automatic as I know, and then use the scraped data combined with the tool - thoughts.

---
+++ Player_Class.py
@@ -0,0 +1,21 @@
+class Player:
+    class Worn_Equipment:
+        def __init__(self):
+            self.head_slot = None
+            self.cape_slot = None
+            self.neck_slot = None
+            self.ammunition_slot = None
+            self.weapon_slot = None
+            self.shield_slot = None
+            self.two_handed_slot = None
+            self.body_slot = None
+            self.legs_slot = None
+            self.hands_slot = None
+            self.feet_slot = None
+
+    class Inventory:
+        def __init__(self):
+            self.Inventory_Slots = [None] * 28
+
+
+
