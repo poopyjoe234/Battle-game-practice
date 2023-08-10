871726a2721c92349bb11b89afa64b8ce9d4f46c
poopyjoe234 <139646796+poopyjoe234@users.noreply.github.com>
2023-08-10 13:58:03 -0400

    update from work pc - worked through a lot fo code with ChatGPT. Comments are made by ChatGPT, they're decent.
    I've coded behaviors for the first item 'Bones'.
    
    This is used by the Monster_Drop_Table.py module,
    and then the testing.py module.
    
    the examine method is not implemented into the main
    code, yet works in the item_behaviors.py module.

--- Monster_Drop_Table.py
+++ Monster_Drop_Table.py
@@ -1,11 +1,9 @@
 import colorama
-
-
-
+import item_behaviors
 class goblin_drops:
     def always_dropped(self, goblin_is_alive):
         print(colorama.Fore.LIGHTGREEN_EX)
-        always_dropped_item = "Bones"
+        always_dropped_item = item_behaviors.goblin_always_dropped.name
         if not goblin_is_alive:
             print(f"Goblin has dropped {always_dropped_item}.")
             print(colorama.Fore.RESET)
--- testing.py
+++ testing.py
@@ -7,65 +7,64 @@ import Monster_Drop_Table
 # Base class for defining common combat attributes for players
 class Player_combat_interface:
     def __init__(self):
-        self.hp = None
-        self.attack_damage = None
-        self.attack_speed = None
+        self.hp = None              # Health Points
+        self.attack_damage = None   # Damage for each attack
+        self.attack_speed = None    # Attack speed (not used in code)
 
 
-# Class to define the attributes and methods of the player
-class Player:
+# Class for the player character
+class Player(Player_combat_interface):
     def __init__(self):
-        self.Player_hp()
-        self.Player_damage()
-        self.Player_attack_speed()
+        super().__init__()
+        self.Player_hp()            # Initialize player's health
+        self.Player_damage()        # Initialize player's attack damage
+        self.Player_attack_speed()  # Initialize player's attack speed
 
     def Player_hp(self):
-        self.hp = 1  # Player's health points
+        self.hp = 1  # Set player's initial health points
 
     def Player_damage(self):
-        self.attack_damage = 1  # Player's attack damage
+        self.attack_damage = 1  # Set player's initial attack damage
 
     def Player_attack_speed(self):
-        self.attack_speed = 1  # Player's attack speed
+        self.attack_speed = 1  # Set player's initial attack speed
 
-    # Method to calculate the chance of deflecting an attack
+    # Calculate chance to deflect an attack and either reduce or increase HP accordingly
     def Player_Deflect_chance(self):
-        Deflect_chance = random.randint(0, 99)
+        Deflect_chance = random.randint(0, 99)  # Generate random chance
         if Deflect_chance > 24:
             print(f"{Deflect_chance + 1} percent.")
             print("The next attack will do damage to the Player.")
             print("----------\n")
             time.sleep(1.5)
-
         else:
-            self.hp += 1  # Increase player's health if deflected
+            self.hp += 1  # Increase player's health if deflected, since no proper deflect method is implemented
             print(f"{Deflect_chance + 1} percent.")
             print("Deflected, the next attack will do no damage to the Player.")
             print("----------\n")
             time.sleep(1.5)
 
-    # Method to handle player's attack on the NPC
+    # Handle player's attack on the NPC, reducing NPC's health
     def Player_attack(self, goblin_class):
         Opponent_hp = goblin_class.hp - self.attack_damage
         goblin_class.hp = Opponent_hp
         print(f"Player has done {self.attack_damage} damage to their opponent.\n")
         time.sleep(1)
 
-    # subclass class for defining player inventory system
-
-
-# subclass class for defining player inventory system
+# Class to manage player's inventory
 class Player_inventory_interface:
     def __init__(self):
         self.inventory = None
 
+    # Initialize player's inventory with two empty slots
     def player_inventory(self):
-        self.inventory = {'Inventory slot 1': 'Empty',
-                          'Inventory slot 2': 'Empty'}
+        self.inventory = {'Inventory slot 1': 'Empty', 'Inventory slot 2': 'Empty'}
 
+    # Print content of the first inventory slot
     def print_inventory(self):
-        print(self.inventory)
+        print(self.inventory['Inventory slot 1'])
 
+    # Handle item pickup after victory against a goblin
     def pickup_item_on_victory(self, goblin_is_alive):
         if not goblin_is_alive:
             pickup = input("Would you like to pickup the dropped items?\n")
@@ -82,40 +81,29 @@ class Player_inventory_interface:
                 else:
                     print("No item to pick up.")
 
+    # Nested class for worn equipment
     class Worn_Equipment:
         def __init__(self):
             self.head_slot = None
-            self.cape_slot = None
-            self.neck_slot = None
-            self.ammunition_slot = None
-            self.weapon_slot = None
-            self.shield_slot = None
-            self.two_handed_slot = None
-            self.body_slot = None
-            self.legs_slot = None
-            self.hands_slot = None
-            self.feet_slot = None
-
+            # More equipment slots defined here...
 
 # Base class for defining common combat attributes for NPCs
 class NPC_combat_interface:
     def __init__(self):
-        self.hp = None
-        self.attack_damage = None
-        self.attack_speed = None
+        self.hp = None              # Health Points
+        self.attack_damage = None   # Damage for each attack
+        self.attack_speed = None    # Attack speed (not used in code)
 
-
-# Class to define the attributes and methods of the goblin
+# Class for the goblin NPC
 class goblin_class(NPC_combat_interface):
-    # Initialization and defining attributes
     def __init__(self):
         super().__init__()
-        self.goblin_hp()
-        self.goblin_attack_speed()
-        self.goblin_attack_damage()
-        self.goblin_is_alive()
+        self.goblin_hp()            # Initialize goblin's health
+        self.goblin_attack_speed()  # Initialize goblin's attack speed
+        self.goblin_attack_damage()  # Initialize goblin's attack damage
+        self.goblin_is_alive()      # Check if goblin is alive
 
-    # Method to handle NPC's attack on the player
+    # Handle goblin's attack on the player, reducing player's health
     def goblin_attack(self, Player):
         Opponent_hp = Player.hp - self.attack_damage
         Player.hp = Opponent_hp
@@ -123,55 +111,56 @@ class goblin_class(NPC_combat_interface):
         time.sleep(1)
 
     def goblin_hp(self):
-        self.hp = 1  # NPC's health points
+        self.hp = 1  # Set goblin's initial health points
 
     def goblin_attack_damage(self):
-        self.attack_damage = 1  # NPC's attack damage
+        self.attack_damage = 1  # Set goblin's initial attack damage
 
     def goblin_attack_speed(self):
-        self.attack_speed = 1  # NPC's attack speed
+        self.attack_speed = 1  # Set goblin's initial attack speed
 
-    # Method to calculate the chance of deflecting an attack by NPC
+    # Calculate chance to deflect an attack and either reduce or increase HP accordingly
     def goblin_Deflect_chance(self):
         while True:
-            Deflect_chance = random.randint(0, 99)
+            Deflect_chance = random.randint(0, 99)  # Generate random chance
             if Deflect_chance > 24:
                 print(f"{Deflect_chance + 1} percent.")
                 print("The next attack will do damage to Goblin.")
                 print("----------\n")
                 time.sleep(1.5)
                 break
-
             else:
-                self.hp += 1  # Increase NPC's health if deflected
+                self.hp += 1  # Increase goblin's health if deflected
                 print(f"{Deflect_chance + 1} percent.")
                 print("Deflected, the next attack will do no damage to Goblin.")
                 print("----------\n")
                 time.sleep(1.5)
                 break
 
+    # Check if goblin is alive based on HP
     def goblin_is_alive(self):
         if self.hp <= 0:
             goblin_is_alive = False
             return goblin_is_alive
 
 
-# Function to handle the battle sequence
+# Function to manage the battle sequence
 def Battle():
-    # Battle loop
+    # Battle loop, continues until one opponent is defeated
     while True:
         print(colorama.Fore.LIGHTWHITE_EX)
         time.sleep(.4)
         print("Battling...\n")
-        Computer_player.goblin_Deflect_chance()
-        Player_player.Player_attack(Computer_player)
+        Computer_player.goblin_Deflect_chance()  # Goblin's chance to deflect
+        Player_player.Player_attack(Computer_player)  # Player's attack on Goblin
         print(colorama.Fore.LIGHTYELLOW_EX)
         print(f"Computer HP: {Computer_player.hp}\n")
         if Computer_player.hp <= 0:
             print("Player has defeated Goblin.\n")
             goblin_is_alive_status = Computer_player.goblin_is_alive()
-            item_to_pickup = goblin_item_drops.always_dropped(goblin_is_alive_status)
-            player_inventory.pickup_item_on_victory(goblin_is_alive_status)
+            goblin_item_drops.always_dropped(goblin_is_alive_status)
+            player_inventory.pickup_item_on_victory(goblin_is_alive_status)  # Pick up item if Goblin defeated
+            player_inventory.print_inventory()  # Print player's inventory
             return 1
         else:
             pass
@@ -179,8 +168,8 @@ def Battle():
         print(colorama.Fore.RED)
         time.sleep(.4)
         print("Battling...\n")
-        Player_player.Player_Deflect_chance()
-        Computer_player.goblin_attack(Player_player)
+        Player_player.Player_Deflect_chance()  # Player's chance to deflect
+        Computer_player.goblin_attack(Player_player)  # Goblin's attack on Player
         print(colorama.Fore.LIGHTYELLOW_EX)
         print(f"Player HP: {Player_player.hp}\n")
         if Player_player.hp <= 0:
@@ -190,24 +179,38 @@ def Battle():
             pass
 
 
-# main loop to run the battle; change the integer in the range function for the amount of loops.
+# Main loop to run the battle; change the integer in the range function for the number of battles
 for i in range(1):
     Player_player_wins = 0
     NPC_wins = 0
 
+# Initialize player inventory, goblin, and player classes
+
+# Create an instance of Player_inventory_interface to manage the player's inventory
 player_inventory = Player_inventory_interface()
+# Call the player_inventory method to initialize the player's inventory with empty slots
 player_inventory.player_inventory()
+
+# Create an instance of the goblin_class to represent the goblin NPC opponent
 Computer_player: goblin_class = goblin_class()
+
+# Retrieve the goblin's item drops from the Monster_Drop_Table module
 goblin_item_drops = Monster_Drop_Table.goblin_drops()
+
+# Create an instance of the Player class to represent the human player character
 Player_player = Player()
+
+# Start the battle sequence by calling the Battle function, and store the result (1 for player win, 2 for NPC win)
 result = Battle()
 
+# Increment win counters based on battle result
 if result == 1:
     Player_player_wins += 1
 else:
     result == 2
     NPC_wins += 1
 
+# Print final battle results
 print(colorama.Fore.LIGHTYELLOW_EX)
 print(f"Player 1 wins: {Player_player_wins}")
 print(f"NPC wins: {NPC_wins}\n")
